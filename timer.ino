249c77e45200b5bd90920a18678af2f8678a60c4
1102-Partovi-Kiana <112733036+1102-Partovi-Kiana@users.noreply.github.com>
2024-05-10 12:11:11 -0700

    Timer Theory for Delay Using Prescalar

---
+++ timer.ino
@@ -0,0 +1,48 @@
+//Timer details for delay 
+
+volatile unsigned char *myTCCR1A = (unsigned char *) 0x80;
+volatile unsigned char *myTCCR1B = (unsigned char *) 0x81;
+volatile unsigned char *myTCCR1C = (unsigned char *) 0x82;
+volatile unsigned char *myTIMSK1 = (unsigned char *) 0x6F;
+volatile unsigned int  *myTCNT1  = (unsigned  int *) 0x84;
+volatile unsigned char *myTIFR1 =  (unsigned char *) 0x36;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+int i = 0;
+
+void loop() {
+  Serial.println(i);
+  i++;
+  my_delay(0.47662); //freq that maximizes delay given a 256 prescaler which is 1.048s
+
+}
+
+void my_delay(unsigned int freq){ //code provided in timer_example.ino
+  //prescaler 
+  const int prescaler = 256; //need to get a longer delay
+  // calc period
+  double period = 1.0/double(freq);
+  // 50% duty cycle
+  double half_period = period/ 2.0f;
+  // clock period def
+  double clk_period = 0.0000000625*prescaler;
+  // calc ticks
+  unsigned int ticks = half_period / clk_period;
+  // stop the timer
+  *myTCCR1B &= 0xF8;
+  // set the counts
+  *myTCNT1 = (unsigned int) (65536 - ticks);
+  // start the timer
+  * myTCCR1A = 0x0;
+  * myTCCR1B |= 0b00000100;//set prescaler
+  // wait for overflow
+  while((*myTIFR1 & 0x01)==0); // 0b 0000 0000
+  // stop the timer
+  *myTCCR1B &= 0xF8;   // 0b 0000 0000
+  // reset TOV           
+  *myTIFR1 |= 0x01;
+}
+
